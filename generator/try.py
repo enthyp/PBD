d95b4fd95b812f1ad7697c7db87aa188817bb508
Jakub <kuba.lanecki@gmail.com>
2019-01-16 13:43:38 +0100

    Adding workshop bookings. Participation to go.

--- generator/try.py
+++ generator/try.py
@@ -21,32 +21,44 @@ with SSHTunnelForwarder((conf.tunnel_host, conf.tunnel_port),
 
             conferences = generator.create_conferences()
             generator.insert_conferences(conferences)
+            print("{} conferences inserted...".format(len(conferences)))
 
             conf_days = generator.create_conference_days(conferences)
             generator.insert_conference_days(conf_days)
+            print("{} conference days inserted...".format(len(conf_days)))
 
             workshops = generator.create_workshops(conf_days)
             generator.insert_workshops(workshops)
+            print("{} workshops inserted...".format(len(workshops)))
 
             prices = generator.create_prices(conferences)
             generator.insert_prices(prices)
+            print("{} prices inserted...".format(len(prices)))
 
             private_clients = generator.create_private_clients()
             company_clients = generator.create_company_clients()
             clients = private_clients + company_clients
             generator.insert_clients(clients)
+            print("{} clients inserted...".format(len(clients)))
 
             attendees = generator.create_attendees()
             generator.insert_attendees(attendees)
+            print("{} attendees inserted...".format(len(attendees)))
 
             bookings = generator.create_bookings(conferences)
             conf_day_bookings = generator.create_conf_day_bookings(bookings, conf_days)
             bookings = generator.remove_empty_bookings(bookings, conf_day_bookings)
 
             generator.insert_bookings(bookings)
+            print("{} conference bookings inserted...".format(len(bookings)))
             generator.insert_conf_day_bookings(conf_day_bookings)
+            print("{} conference day bookings inserted...".format(len(conf_day_bookings)))
 
-            cursor.execute("SELECT * FROM CONF_DAY_BOOKINGS")
+            workshop_bookings = generator.create_workshop_bookings(conf_day_bookings, workshops)
+            generator.insert_workshop_bookings(workshop_bookings)
+            print("{} workshop bookings inserted...".format(len(workshop_bookings)))
+
+            cursor.execute("SELECT * FROM WORKSHOP_BOOKINGS")
             pprint(cursor.fetchmany(10))
         except cx_Oracle.DatabaseError as e:
             print('Database error upon execution: ' + str(e))
